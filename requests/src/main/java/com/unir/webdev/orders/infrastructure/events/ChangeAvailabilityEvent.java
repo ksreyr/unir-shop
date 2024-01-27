b7228856afb089c44a810b92671672c6845b868c
Kevin Santiago Rey <ksreyr@gmail.com>
2024-01-27 15:25:54 +0100

    feat(request): add patch support for rest client

--- requests/src/main/java/com/unir/webdev/orders/infrastructure/events/ChangeAvailabilityEvent.java
+++ requests/src/main/java/com/unir/webdev/orders/infrastructure/events/ChangeAvailabilityEvent.java
@@ -7,7 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,11 +23,11 @@ import java.util.UUID;
 public class ChangeAvailabilityEvent implements ResquestEvents {
     RestTemplate restTemplate;
     public void sendEventChangeAvailability(List<UUID> booksList){
-        String url = "http://books:8082/api/v1/books/changeAvailability";
+        String url = "http://books:8082/api/v1/books/availability";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         var requestEntity = new HttpEntity<>(new RequestChangeBookAvailability(booksList), headers);
-        String s = restTemplate.postForObject(url, requestEntity, String.class);
-        System.out.println(s);
+        var s = restTemplate.exchange(url, HttpMethod.PATCH, requestEntity, String.class);
+        System.out.println(s.getBody());
     }
 }
