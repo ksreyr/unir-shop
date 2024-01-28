156d66cc87f2ae8161680d1443f3761475f27e12
jdavellanedag <jdavellaendag@gmail.com>
2024-01-28 23:48:49 +0100

    Add missing request and fix delete request method

--- requests/src/main/java/com/unir/webdev/orders/infrastructure/controllers/dto/DeleteRequest.java
+++
@@ -1,9 +0,0 @@
-package com.unir.webdev.orders.infrastructure.controllers.dto;
-
-import java.util.UUID;
-
-public record DeleteRequest(UUID requestUUID) {
-    public boolean isNotNull() {
-        return requestUUID != null;
-    }
-}
